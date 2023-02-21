b6445ac6696e05ecddf536c42c329492394e94b6
abdulbari149 <abdulbari131103@gmail.com>
2023-02-21 23:05:57 +0500

    index route for confirmation

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,9 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.status(200).json({
+        message: 'Welcome to the backend of social media app',
+      });
     } catch (error) {
       next(error);
     }
